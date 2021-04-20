f0a9f5a34fecbddc8b1fa06d44eda6d240c130bb
Roberto Ribes <roberto.ribes@tgw-group.com>
2021-04-20 09:46:31 +0200

    More things learned from React

--- MazeMapper.Web.Solution/MazeMapper.Web.Application/ClientApp/src/components/Layout.js
+++ MazeMapper.Web.Solution/MazeMapper.Web.Application/ClientApp/src/components/Layout.js
@@ -5,6 +5,7 @@ import { NavMenu } from './NavMenu';
 export class Layout extends Component {
   static displayName = Layout.name;
 
+  // this.props.children displays all children elements that are defined in App.js between the opening and closing tag <Layout> - </Layout> (where this components is used)
   render () {
     return (
       <div>
