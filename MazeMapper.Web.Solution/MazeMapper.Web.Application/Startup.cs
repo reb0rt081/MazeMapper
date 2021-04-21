6dd5ff748a49e0e2e0ecfa94c8a403b971c31306
Roberto Ribes <roberto.ribes@tgw-group.com>
2021-04-21 14:09:41 +0200

    Base maze map branch ready

--- MazeMapper.Web.Solution/MazeMapper.Web.Application/Startup.cs
+++ MazeMapper.Web.Solution/MazeMapper.Web.Application/Startup.cs
@@ -23,6 +23,7 @@ namespace MazeMapper.Web.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //  Injecttion of dependency
             services.AddSingleton<IMazeMapManager, MazeMapManager>();
 
             services.AddControllersWithViews();
