6b8238a70fb74ae5f18964d8ee0a2093e191c304
Roberto Ribes <roberto.ribes@tgw-group.com>
2021-04-21 09:34:40 +0200

    Removed some unnecessary files

--- MazeMapper.Web.Solution/MazeMapper.Web.FrontEnd/ClientApp/src/App.js
+++ MazeMapper.Web.Solution/MazeMapper.Web.FrontEnd/ClientApp/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import { MazeMap } from './components/MazeMap';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -16,8 +14,6 @@ export default class App extends Component {
       return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/maze-map' component={MazeMap} />
       </Layout>
     );
